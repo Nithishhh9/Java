9548d15b2f59b563bbdd0ba210f5750e223215a0
Nithishhh9 <nithishhh77@gmail.com>
2024-08-03 18:04:37 +0530

    Initial commit

---
+++ bleak.java
@@ -0,0 +1,52 @@
+package Java;
+import java.io.*;
+import java.util.*;
+public class bleak{
+    static int countSetBits(int x)
+    {
+        int count = 0;
+        while (x != 0) {
+            x &= (x - 1);
+            count++;
+        }
+        return count;
+    }
+ 
+    // A function to return ceiling of log x
+    // in base 2. For example, it returns 3
+    // for 8 and 4 for 9.
+    static int ceilLog2(int x)
+    {
+        int count = 0;
+        x--;
+        while (x > 0) {
+            x = x >> 1;
+            count++;
+        }
+        return count;
+    }
+ 
+    // Returns true if n is Bleak
+    static boolean isBleak(int n)
+    {
+        // Check for all numbers 'x' smaller
+        // than n. If x + countSetBits(x)
+        // becomes n, then n can't be Bleak
+        for (int x = n - ceilLog2(n); x < n; x++)
+            if (x + countSetBits(x) == n)
+                return false;
+ 
+        return true;
+    }
+ 
+    // Driver code
+    public static void main(String[] args)
+    {
+        if (isBleak(5))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+ 
+    }
+}
+// }

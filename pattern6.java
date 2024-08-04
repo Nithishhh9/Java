d6c53a18c08c1ccb8e494a04d155ec58564e5c3a
Nithishhh9 <nithishhh77@gmail.com>
2024-08-04 23:42:49 +0530

    commit

---
+++ pattern6.java
@@ -0,0 +1,20 @@
+package Java;
+
+import java.util.Scanner;
+
+public class pattern6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number :");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            int start=0;
+            if(i%2==0) start=1;
+            for(int j=0;j<=i;j++){
+                System.out.print(start+" ");
+                start=1-start;
+            }
+            System.out.println();
+        }
+    }
+}

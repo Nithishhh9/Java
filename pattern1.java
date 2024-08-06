c68c1139d3fe4e49c1ff2e733301d5e0d6984377
Nithishhh9 <nithishhh77@gmail.com>
2024-08-06 19:30:07 +0530

    commit

--- pattern1.java
+++ pattern1.java
@@ -11,7 +11,7 @@ public class pattern1 {
         for(int i=0;i<n;i++){
             for(int j=0;j<n-i;j++){
                 System.out.print("* ");
-            }   
+            }      
             System.out.println();
         }
     }

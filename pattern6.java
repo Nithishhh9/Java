63c406881a414cc589f859f6d112e8ee4d29fed3
Nithishhh9 <nithishhh77@gmail.com>
2024-08-10 23:05:45 +0530

    Update pattern6.java

--- pattern6.java
+++ pattern6.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class pattern6 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter number :");
+        System.out.print("Enter number :"); 
+
         int n=sc.nextInt();
+        
         for(int i=0;i<n;i++){
             int start=0;
             if(i%2 ==0) start=1;

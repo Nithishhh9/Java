b31645f44ef84c4acd9c5cde1c53aab20e869d92
Nithishhh9 <nithishhh77@gmail.com>
2024-08-17 22:36:11 +0530

    Update pattern2.java

--- pattern2.java
+++ pattern2.java
@@ -6,9 +6,9 @@ public class pattern2 {
     public static void main(String[] args) {
         Scanner Sc=new Scanner(System.in);
         System.out.println("Enetr number :");
-        int n=Sc.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n-i;j++){
+        int num=Sc.nextInt();
+        for(int i=0;i<num;i++){
+            for(int j=0;j<num-i;j++){
                 System.out.print(j+1  +" ");
             }
             System.out.println();

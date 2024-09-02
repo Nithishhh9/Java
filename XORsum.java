475b9e0310d8c09cdb29fff4a334290bdfdc1edc
Nithishhh9 <nithishhh77@gmail.com>
2024-09-02 18:29:38 +0530

    commit

---
+++ XORsum.java
@@ -0,0 +1,31 @@
+package Java;
+
+import java.util.Scanner;
+
+public class XORsum {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int res = Xorsum(arr,n);
+        System.out.println(res);
+
+
+
+    }
+    static int Xorsum(int [] arr,int n){
+        int x,y=0;
+        for(int i=0;i<n;i++){
+            if( i ^ 1 == i+1){
+                x = x^arr[i];
+            }
+            else{
+             y =y+arr[i];
+            }
+        }
+        return y-x;
+    }
+}
---
+++ maxpairproduct.java
@@ -0,0 +1,37 @@
+package Java;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class maxpairproduct {
+     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int [] result = maxprod(arr,n);
+        for(int i=0;i<2;i++){
+            System.out.print(result[i]+" ");
+        }
+        sc.close();
+    }
+    static int[] maxprod(int[] arr,int n){
+        long prod=0,max=0;
+        int[] res=new int[2];
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0;i<n;i++){
+            if(arr[i] !=9 && hs.contains(18-arr[i])){
+                prod =(long)(arr[i] * (18-arr[i]));
+                if(prod>max){
+                    max=prod;
+                    res[0]=Math.max(arr[i],(18-arr[i]));
+                    res[1]=Math.min(arr[i],(18-arr[i]));
+                }
+            }
+            hs.add(arr[i]);
+        }
+        return res;
+    }
+}

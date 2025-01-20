45e204504e70e53bb42eb17d1c2842cf8a165e64
nithishhh9 <nithishhh77@gmail.com>
2025-01-20 21:39:15 +0530

    Create acendion.java

---
+++ acendion.java
@@ -0,0 +1,33 @@
+// package .vscode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class acendion {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("emter size");
+        int n=sc.nextInt();
+        System.out.println("enter Array");
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+ 
+        int result=defected(arr,n);
+        System.out.println(result);
+    }
+
+    public static int defected(int arr[],int n){
+        int count=0;
+        Arrays.sort(arr);
+        for(int i=0;i<n-1;i++){
+            int dif=arr[i+1]-arr[i];
+            if(dif>1){
+                count+=dif-1;
+            }
+        }
+
+        return count;
+    }
+}
